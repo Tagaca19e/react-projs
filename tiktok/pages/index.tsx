3ce971c1a88d285c18605a78565f05704ce4a57c
Eidmone Tagaca <etagaca@csub.edu>
2022-12-28 21:02:11 -0800

    Add Sanity.io connection

--- tiktok/pages/index.tsx
+++ tiktok/pages/index.tsx
@@ -2,10 +2,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import axios from 'axios';
+import { Video } from '../../types';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+interface IProps {
+  videos: Video[]
+}
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
+}
+
+export default function Home({ videos }: IProps) {
+  console.log(videos);
   return (
     <h1 className="text-3xl font-bold underline">
       TikTok
